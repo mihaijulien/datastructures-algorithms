e44bde9b5c72dba7e5fb2f5a58a0b77556f6565c
Julien Mihai <mihaijulien@gmail.com>
2021-05-29 19:11:56 +0300

    doc: javadoc comments for sorting algortihms

--- src/main/java/mihaijulien/eu/algorithms/sorting/InsertionSort.java
+++ src/main/java/mihaijulien/eu/algorithms/sorting/InsertionSort.java
@@ -1,10 +1,21 @@
 package mihaijulien.eu.algorithms.sorting;
 
+/**
+ * Insertion sort iterates, consuming one input element each repetition, and grows a sorted output list. At each iteration,
+ * insertion sort removes one element from the input data, finds the location it belongs within the sorted list, and
+ * inserts it there. It repeats until no input elements remain.
+ *
+ * @see <a href="https://en.wikipedia.org/wiki/Insertion_sort">https://en.wikipedia.org/wiki/Insertion_sort</a>
+ */
 public class InsertionSort {
 
     private InsertionSort() {
     }
 
+    /**
+     * @param array the array of values to sort
+     * @return the sorted array
+     */
     public static int[] sort(int[] array) {
         for (int i = 1; i < array.length; i++) {
             for (int j = i; j > 0; j--) {
--- src/main/java/mihaijulien/eu/algorithms/sorting/SelectionSort.java
+++ src/main/java/mihaijulien/eu/algorithms/sorting/SelectionSort.java
@@ -1,10 +1,24 @@
 package mihaijulien.eu.algorithms.sorting;
 
+/**
+ * The algorithm divides the input list into two parts: a sorted sublist of items which is built up from left to right
+ * at the front (left) of the list and a sublist of the remaining unsorted items that occupy the rest of the list.
+ * Initially, the sorted sublist is empty and the unsorted sublist is the entire input list. The algorithm proceeds by
+ * finding the smallest (or largest, depending on sorting order) element in the unsorted sublist, exchanging (swapping)
+ * it with the leftmost unsorted element (putting it in sorted order), and moving the sublist boundaries one element
+ * to the right.
+ *
+ * @see <a href="https://en.wikipedia.org/wiki/Selection_sort">https://en.wikipedia.org/wiki/Selection_sort</a>
+ */
 public class SelectionSort {
 
     private SelectionSort() {
     }
 
+    /**
+     * @param array the array of values to sort
+     * @return the sorted array
+     */
     public static int[] sort(int[] array) {
         for (int i = 0; i < array.length; i++) {
             int minimum = i;
