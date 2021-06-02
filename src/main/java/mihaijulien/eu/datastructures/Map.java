28df3bc869a4a7a7d8a17c65a03fb99b9f469241
Julien Mihai <mihaijulien@gmail.com>
2021-06-02 14:47:06 +0300

    feat: map and bst

---
+++ src/main/java/mihaijulien/eu/datastructures/Map.java
@@ -0,0 +1,92 @@
+package mihaijulien.eu.datastructures;
+
+public class Map<K, V> {
+    private static final int MAX_SIZE = 10;
+
+    private int size;
+    private Entry<K, V>[] map;
+
+    public Map() {
+        size = 0;
+        map = new Entry[MAX_SIZE];
+    }
+
+    public void put(K key, V value) {
+        boolean insert = true;
+        if (isFull()) {
+            throw new IllegalStateException("Map is full");
+        }
+        for (int i = 0; i < size; i++) {
+            if (key.equals(map[i].getKey())) {
+                insert = false;
+            }
+        }
+        if (!isFull() && insert) {
+            map[size] = new Entry<>(key, value);
+        }
+        size++;
+    }
+
+    public void remove(K key) {
+        for (int i = 0; i < size; i++) {
+            if (key.equals(map[i].getKey())) {
+                map[i] = null;
+                size--;
+            }
+        }
+    }
+
+    public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if (map[i].getKey().equals(key)) {
+                return map[i].getValue();
+            }
+        }
+        return null;
+    }
+
+    public boolean containsKey(K key) {
+        for (int i = 0; i < size; i++) {
+            if (map[i].getKey().equals(key)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean containsValue(V value) {
+        for (int i = 0; i < size; i++) {
+            if (map[i].getValue().equals(value)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isFull() {
+        return size == MAX_SIZE;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+
+    private static class Entry<K, V> {
+        private final K key;
+        private final V value;
+
+        public Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+    }
+}
---
+++ src/test/java/mihaijulien/eu/datastructures/BinarySearchTreeTest.java
@@ -0,0 +1,41 @@
+package mihaijulien.eu.datastructures;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BinarySearchTreeTest {
+
+    BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+
+    @Before
+    public void setUp(){
+
+         /*
+              52
+           /     \
+          15      56
+         /  \     /  \
+        9    11  54   61
+       / \
+      3   5 */
+
+        bst.insert(52);
+        bst.insert(15);
+        bst.insert(56);
+        bst.insert(15);
+        bst.insert(56);
+        bst.insert(9);
+        bst.insert(16);
+        bst.insert(54);
+        bst.insert(3);
+        bst.insert(10);
+        bst.insert(65);
+        bst.insert(72);
+        bst.insert(61);
+    }
+
+    @Test
+    public void deleteNodeFromBSTTest(){
+        bst.delete(56);
+    }
+}
